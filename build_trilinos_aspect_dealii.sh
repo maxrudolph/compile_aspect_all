c1daafeec6cc95c6888021f9dc785a308aa7b4e3
Max Rudolph @ Coeus <rmaxwell@pdx.edu>
2017-04-12 17:08:45 -0700

    add build script

---
+++ build_trilinos_aspect_dealii.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+# Build script to install p4est, trilinos, deal.ii, and aspect on cluster.
+# Modified from deal.ii dockerfiles.
+
+BUILD_DIR=$PWD
+INSTALL_PREFIX=/home/rmaxwell/libs
+CPU=20
+
+# load modules
+module purge
+module load openmpi-2.0/gcc-6.3.0
+
+# create the installation directory for all programs
+mkdir $INSTALL_PREFIX
+
+# download and build p4est
+cd $BUILD_DIR
+export P4EST_VERSION=1.1
+wget http://p4est.github.io/release/p4est-$P4EST_VERSION.tar.gz && \
+wget http://www.dealii.org/developer/external-libs/p4est-setup.sh && \
+    chmod +x p4est-setup.sh && \
+    ./p4est-setup.sh p4est-$P4EST_VERSION.tar.gz $INSTALL_PREFIX/p4est-$P4EST_VERSION && \
+    rm -rf p4est-build p4est-$P4EST_VERSION p4est-setup.sh p4est-$P4EST_VERSION.tar.gz && \
+
+export P4EST_DIR=$INSTALL_PREFIX/p4est-$P4EST_VERSION && \
+# download and build hdf5
+export HDF5_VERSION=1.10.0-patch1 && \
+cd $BUILD_DIR && \
+wget http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-$HDF5_VERSION.tar.bz2 && \
+    tar xjf hdf5-$HDF5_VERSION.tar.bz2 && \
+    cd hdf5-$HDF5_VERSION &&  \
+    ./configure --enable-parallel \
+                --enable-shared \
+                --prefix=$INSTALL_PREFIX/hdf5-$HDF5_VERSION/ && \
+    make -j$CPU && make install && \
+    cd $BUILD_DIR && \
+    rm -rf hdf5-$HDF5_VERSION hdf5-$HDF5_VERSION.tar.bz2 
+export HDF5_DIR=$INSTALL_PREFIX/hdf5-$HDF5_VERSION
+
+# download and build trilinos
+export TRILINOS_VERSION=12-8-1
+cd $BUILD_DIR
+wget https://github.com/trilinos/Trilinos/archive/trilinos-release-$TRILINOS_VERSION.tar.gz && \
+    tar xfz trilinos-release-$TRILINOS_VERSION.tar.gz && \
+    mkdir Trilinos-trilinos-release-$TRILINOS_VERSION/build && \
+    cd Trilinos-trilinos-release-$TRILINOS_VERSION/build && \
+    cmake \
+     -D BUILD_SHARED_LIBS=ON \
+     -D CMAKE_BUILD_TYPE=RELEASE \
+     -D CMAKE_CXX_FLAGS="-O3" \
+     -D CMAKE_C_FLAGS="-O3" \
+     -D CMAKE_FORTRAN_FLAGS="-O5" \
+     -D CMAKE_INSTALL_PREFIX:PATH=$INSTALL_PREFIX/trilinos-$TRILINOS_VERSION \
+     -D CMAKE_VERBOSE_MAKEFILE=FALSE \
+     -D TPL_ENABLE_Boost=OFF \
+     -D TPL_ENABLE_MPI=ON \
+     -D TPL_ENABLE_Netcdf:BOOL=OFF \
+     -D TrilinosFramework_ENABLE_MPI:BOOL=ON \
+     -D Trilinos_ASSERT_MISSING_PACKAGES:BOOL=OFF \
+     -D Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=ON \
+     -D Trilinos_ENABLE_ALL_PACKAGES:BOOL=OFF \
+     -D Trilinos_ENABLE_Amesos:BOOL=ON \
+     -D Trilinos_ENABLE_AztecOO:BOOL=ON \
+     -D Trilinos_ENABLE_Epetra:BOOL=ON \
+     -D Trilinos_ENABLE_EpetraExt:BOOL=ON \
+     -D Trilinos_ENABLE_Ifpack:BOOL=ON \
+     -D Trilinos_ENABLE_Jpetra:BOOL=ON \
+     -D Trilinos_ENABLE_Kokkos:BOOL=ON \
+     -D Trilinos_ENABLE_Komplex:BOOL=ON \
+     -D Trilinos_ENABLE_ML:BOOL=ON \
+     -D Trilinos_ENABLE_MOOCHO:BOOL=ON \
+     -D Trilinos_ENABLE_MueLu:BOOL=ON \
+     -D Trilinos_ENABLE_OpenMP:BOOL=OFF \
+     -D Trilinos_ENABLE_Piro:BOOL=ON \
+     -D Trilinos_ENABLE_Rythmos:BOOL=ON \
+     -D Trilinos_ENABLE_STK:BOOL=OFF \
+     -D Trilinos_ENABLE_Sacado=ON \
+     -D Trilinos_ENABLE_TESTS:BOOL=OFF \
+     -D Trilinos_ENABLE_Stratimikos=ON \
+     -D Trilinos_ENABLE_Teuchos:BOOL=ON \
+     -D Trilinos_ENABLE_Thyra:BOOL=ON \
+     -D Trilinos_ENABLE_Tpetra:BOOL=ON \
+     -D Trilinos_ENABLE_TrilinosCouplings:BOOL=ON \
+     -D Trilinos_EXTRA_LINK_FLAGS="-lgfortran" \
+     -D Trilinos_VERBOSE_CONFIGURE=FALSE \
+     .. && \
+   make -j$CPU && make install && \
+   cd $BUILD_DIR && \
+   rm -rf Trilinos-trilinos-release-* &&\
+   rm -rf trilinos-release-*
+export TRILINOS_DIR=$INSTALL_PREFIX/trilinos-$TRILINOS_VERSION
+
+VER=master
+BUILD_TYPE=DebugRelease
+cd $BUILD_DIR && \
+git clone https://github.com/dealii/dealii.git dealii-$VER-src && \
+    cd dealii-$VER-src && \
+    git checkout $VER && \
+    mkdir build && cd build && \
+    cmake -DDEAL_II_WITH_MPI=ON \
+          -DDEAL_II_COMPONENT_EXAMPLES=OFF \
+          -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX/dealii-$VER \
+          -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+          -DTRILINOS_DIR=$TRILINOS_DIR \
+          -DP4est_DIR=$P4EST_DIR \
+          ../ && \
+    make -j$CPU install && \
+    cd $BUILD_DIR && rm -rf dealii-$VER-src && \
+
+export DEAL_II_DIR=$INSTALL_PREFIX/dealii-$VER
+
+cd $BUILD_DIR && \
+git clone https://github.com/geodynamics/aspect.git aspect-src && \
+    cd aspect-src && \
+    git checkout master && \
+    mkdir build && cd build && \
+    cmake -DDEAL_II_DIR=$DEAL_II_DIR \
+          -DCMAKE_BUILD_TYPE=Release \
+    ..
+    make -j$CPU
+    mkdir $INSTALL_PREFIX/aspect
+    mv aspect $INSTALL_PREFIX/aspect.fast
+    cd .. && \
+	rm -rf build
+    mkdir build && cd build && \
+	cmake -DDEAL_II_DIR=$DEAL_II_DIR \
+        -DCMAKE_BUILD_TYPE=Debug \
+	..
+    make -j$CPU && \
+	mv aspect $INSTALL_PREFIX/aspect.debug 
+    cd .. && \
+	rm -rf build
+    cd $BUILD_DIR && \
+	rm -rf aspect-src
+    
